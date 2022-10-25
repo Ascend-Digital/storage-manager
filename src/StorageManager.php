1f007410ae6ef8a3ed61af1c2506c2ffb84a534e
TKAscend <83606571+TKAscend@users.noreply.github.com>
2022-10-25 11:59:46 +0100

    Creates uploadFileFromLocation function

--- src/StorageManager.php
+++ src/StorageManager.php
@@ -3,11 +3,41 @@
 namespace AscendDigital\StorageManager;
 
 use AscendDigital\StorageManager\Models\FileUpload;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class StorageManager implements Contracts\StorageManager
 {
+    /**
+     * With the given location, creates a FileUpload from it.
+     *
+     * Only supports local locations for now.
+     *
+     * Example Usage:
+     *
+     * StorageManager::uploadFileFromLocation(
+     *  storage_path('app/public/files/measuring-shutters/Back_of_Bay_Measure_Guide.pdf'),
+     *  'public/files/measuring-shutters'
+     * );
+     *
+     * Which gives you a FileUpload object with the following url:
+     * /storage/files/measuring-shutters/jZXfVAPA6Z8eEqVYF9Kt7AxOiCaLPv5WSz4EfN4f.pdf
+     *
+     * You may choose to provide a filepath from tmp directory to be placed into the project
+     *
+     * @param  string  $location
+     * @param  string|null  $tag
+     *
+     * @return FileUpload|null
+     */
+    public function uploadFileFromLocation(string $location, string $tag = null): ?FileUpload
+    {
+        $file = new UploadedFile($location, basename($location));
+
+        return $this->uploadFile($file, $tag);
+    }
+
     /**
      * {@inheritdoc}
      */
